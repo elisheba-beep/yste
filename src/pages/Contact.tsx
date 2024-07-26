4ad46df8757c68b89b262e3800f0db94f2c4ad6d
elisheba-beep <ekunife35@gmail.com>
2024-07-26 09:36:45 +0100

    form validation updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,6 +11,12 @@ export default function Contact() {
     message: "",
   };
   const [formData, setFormData] = useState(initialFormData);
+  const [errors, setErrors] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    message: "",
+  });
 
   const handleChange = (event: {
     target: { name: string; value: string | number };
@@ -22,12 +28,67 @@ export default function Contact() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSubmit = (event: { preventDefault: any }) => {
     event.preventDefault();
-    console.log(
-      `Full name: ${formData.fullname}, Email: ${formData.email}, Message: ${formData.message}`
-    );
-    resetForm();
+    const newErrors = validateForm(formData);
+    setErrors(newErrors);
+    let noErr = 0;
+    for (let i = 0; i < Object.values(newErrors).length; i++) {
+      if (Object.values(newErrors)[i].length != 0) {
+        noErr++;
+      }
+    }
+    if (noErr === 0) {
+      alert(
+        `Full name: ${formData.fullname}, Email: ${formData.email}, Message: ${formData.message}`
+      );
+      console.log("Form submitted successfully!");
+      resetForm();
+    } else {
+      console.log(`Form submission failed
+             due to validation errors.`);
+    }
+    console.log(Object.values(newErrors));
+    console.log(noErr);
+  };
+  // validate form
+  const validateForm = (data: {
+    fullname: string;
+    email: string;
+    phoneNumber: string;
+    subject?: string;
+    message: string;
+    username?: string;
+  }) => {
+    const errors = {
+      fullname: "",
+      email: "",
+      phoneNumber: "",
+      message: "",
+    };
+
+    if (!data.fullname.trim()) {
+      errors.fullname = "Name is required";
+    }
+
+    if (!data.email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+      errors.email = "Email is invalid";
+    }
+
+    if (!data.phoneNumber) {
+      errors.phoneNumber = "Password is required";
+    } else if (data.phoneNumber.length !== 11) {
+      errors.phoneNumber = `Phone number must be 11 characters long`;
+    }
+
+    if (!data.message) {
+      errors.message = "Message is required";
+    }
+
+    return errors;
   };
 
+  // reset form
   const resetForm = () => {
     setFormData(initialFormData);
   };
@@ -64,22 +125,36 @@ export default function Contact() {
 
           <form onSubmit={handleSubmit}>
             <div className="flex gap-2">
-              <input
-                type="text"
-                name="fullname"
-                value={formData.fullname}
-                onChange={handleChange}
-                placeholder="Your full name*"
-                className="border-gray-200 my-2 outline-none border-2 rounded-md p-2 w-1/2"
-              />
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Write your email here*"
-                className="border-gray-200 my-2 outline-none border-2 rounded-md p-2 w-1/2"
-              />
+              <div>
+                <input
+                  type="text"
+                  name="fullname"
+                  value={formData.fullname}
+                  onChange={handleChange}
+                  placeholder="Your full name*"
+                  className="border-gray-200 my-2 outline-none border-2 rounded-md p-2"
+                />
+                {errors.fullname && (
+                  <p className="text-red-500 font-primaryRegular">
+                    {errors.fullname}
+                  </p>
+                )}
+              </div>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Write your email here*"
+                  className="border-gray-200 my-2 outline-none border-2 rounded-md p-2"
+                />
+                {errors.email && (
+                  <p className="text-red-500 font-primaryRegular">
+                    {errors.email}
+                  </p>
+                )}
+              </div>
             </div>
             <input
               type="tel"
@@ -92,6 +167,11 @@ export default function Contact() {
               placeholder="Phone number*"
               className="border-gray-200 my-2 outline-none border-2 rounded-md p-2 w-full"
             />
+            {errors.phoneNumber && (
+              <span className="text-red-500 font-primaryRegular">
+                {errors.phoneNumber}
+              </span>
+            )}
             <input
               type="text"
               name="subject"
@@ -108,12 +188,19 @@ export default function Contact() {
               placeholder="Write your message here*"
               className="border-gray-200 my-2 outline-none border-2 rounded-md p-2 w-full"
             />
-            <button
-              type="submit"
-              className="rounded-3xl w-1/6 h-10 bg-white text-black border-2 border-gray-700 hover:text-white hover:bg-black my-4"
-            >
-              SUBMIT
-            </button>
+            {errors.message && (
+              <span className="text-red-500 font-primaryRegular">
+                {errors.message}
+              </span>
+            )}
+            <div>
+              <button
+                type="submit"
+                className="rounded-3xl w-1/6 h-10 bg-white text-black border-2 border-gray-700 hover:text-white hover:bg-black my-4"
+              >
+                SUBMIT
+              </button>
+            </div>
           </form>
         </div>
 
